aa2ae00c2def106bb0bedc7c4dcfea05a5407abb
崔斌 <cuiminmm@gmail.com>
2017-08-07 00:57:48 +0800

    update

--- chaincode-docker-devmode/script.sh
+++ chaincode-docker-devmode/script.sh
@@ -25,5 +25,7 @@ peer channel join -b myc.block
 CORE_PEER_ADDRESS=peer:7051 CORE_CHAINCODE_ID_NAME=charity:0 /opt/gopath/src/chaincodedev/chaincode/charity/charity &
 peer chaincode install -p chaincodedev/chaincode/charity -n charity -v 0
 peer chaincode instantiate -n charity -v 0 -c '{"Args":[]}' -C myc
+cd $GOPATH/src/api_charity
+bee run 
 sleep 600000
 exit 0
